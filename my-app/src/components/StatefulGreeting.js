51592aaf5584c2894d4450852f5767494f8c737a
pozhara <poliazhara04@gmail.com>
2023-07-28 15:06:03 +0000

    Using class component lesson

---
+++ my-app/src/components/StatefulGreeting.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+class StatefulGreeting extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            introduction: 'Hello',
+            buttonText: 'Exit',
+        };
+    }
+
+    handleClick(){
+        this.setState({
+            introduction:"Goodbye!"
+        })
+    }
+
+    render(){
+        return (
+            <div>
+                <h1>{this.state.introduction}, {this.props.greeting}</h1>
+                <button onClick={() => this.handleClick()}>{this.state.buttonText}</button>
+            </div>
+        )
+    }
+}
+
+export default StatefulGreeting
